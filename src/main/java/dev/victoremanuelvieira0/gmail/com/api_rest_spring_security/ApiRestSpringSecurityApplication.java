616647e597b77a6c254eea9b238e5fafb45043aa
Victor Emanuel <victoremanuelvieira0@gmail.com>
2024-11-01 08:42:15 -0300

    iniciando projeto

---
+++ src/main/java/dev/victoremanuelvieira0/gmail/com/api_rest_spring_security/ApiRestSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.victoremanuelvieira0.gmail.com.api_rest_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/victoremanuelvieira0/gmail/com/api_rest_spring_security/ApiRestSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.victoremanuelvieira0.gmail.com.api_rest_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
