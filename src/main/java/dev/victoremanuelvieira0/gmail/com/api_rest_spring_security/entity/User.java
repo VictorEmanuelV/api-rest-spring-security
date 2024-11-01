c596787fd55e3f173fa7ff1155c59b4921a3ac39
Victor Emanuel <victoremanuelvieira0@gmail.com>
2024-11-01 09:36:32 -0300

    entity user

---
+++ src/main/java/dev/victoremanuelvieira0/gmail/com/api_rest_spring_security/entity/User.java
@@ -0,0 +1,28 @@
+package dev.victoremanuelvieira0.gmail.com.api_rest_spring_security.entity;
+import dev.victoremanuelvieira0.gmail.com.api_rest_spring_security.enums.RoleEnum;
+import jakarta.persistence.*;
+import lombok.*;
+import java.io.Serializable;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "TB_USER")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+    @Column(name = "password",nullable = false)
+    private String password;
+    @Column(name = "role",nullable = false)
+    private RoleEnum role;
+
+}
---
+++ src/main/java/dev/victoremanuelvieira0/gmail/com/api_rest_spring_security/enums/RoleEnum.java
@@ -0,0 +1,15 @@
+package dev.victoremanuelvieira0.gmail.com.api_rest_spring_security.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RoleEnum {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    RoleEnum(String role){
+        this.role = role;
+    }
+}
